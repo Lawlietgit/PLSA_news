45a7ea15ff6b9a1c241d9437cda5977dc0e593be
Qi Li <q.l.lawliet@gmail.com>
2020-07-05 20:00:47 +0000

    more stopwords

--- word_split/_core.py
+++ word_split/_core.py
@@ -12,9 +12,23 @@ import hanlp
 import re
 from pyspark.sql.types import StringType
 from pyspark.sql.functions import udf, col
+import codecs
 
 logger = logging.getLogger(__name__)
 
+
+tokenizer = hanlp.load('PKU_NAME_MERGED_SIX_MONTHS_CONVSEG')
+#tokenizer = hanlp.load('CTB6_CONVSEG')
+
+remove_set = set(["?", ":", ":", "万得", "通讯社", "报道", "年", "月", "日", "年月日", "日晚", "年月",
+                  "月日", "新浪财"])
+lines = codecs.open('/home/hadoop/PLSA_news/data/chinese_stopwords.txt', 'r', 'utf-8').readlines()
+stopwords = set()
+for line in lines:
+    words = line.replace('\n','').split(',')
+    words = [re.sub(r'\s+', '', w) for w in words if w!='']
+    stopwords = stopwords.union(set(words))
+kill_set = remove_set.union(stopwords)
 
 def get_text(c, html=True):
     if html:
@@ -23,8 +37,9 @@ def get_text(c, html=True):
     else:
         text = c
     p = r"[\s+\.\!\/_,$%^*(+\"\']+|[+——！，？?、~@#￥%……&*（）【】《》℃丨“”(a-z)(0-9)(A-Z)：-]"
-    tokenizer = hanlp.load('PKU_NAME_MERGED_SIX_MONTHS_CONVSEG')
-    return ' '.join(tokenizer(re.sub(p, '', text))).strip()
+    words = tokenizer(re.sub(p, '', text))
+    words = [w for w in words if w not in kill_set] 
+    return ' '.join(words).strip()
     
 
 getText_udf = udf(lambda x: get_text(x), StringType())
@@ -39,50 +54,14 @@ def run(spark=None):
     df = df.withColumn("clean_title", getTitle_udf(df.TITLE))
     df = df.select(['OBJECT_ID', 'WINDCODES', 'clean_title', 'clean_text'])
     df.show()
+    print('hanlp finished!')
     print(df.count())
     df.printSchema()
-#    outpath = 's3://qi-plsa-test/output/word_split_sample.parquet'
-#    write_parquet_spark(df, outpath, coalesce=True)
-#    df = read_parquet_spark(spark, outpath)
-    outpath = 'word_split_sample.csv'
-    write_csv_spark(df, outpath)
-    df = read_csv_spark(spark, outpath)
+    outpath = 's3://qi-plsa-test/output/word_split_sample.parquet'
+    write_parquet_spark(df, outpath, coalesce=True)
+    df = read_parquet_spark(spark, outpath)
+    #outpath = 'word_split_sample.csv'
+    #write_csv_spark(df, outpath)
+    #df = read_csv_spark(spark, outpath)
     df.printSchema()
     df.show()
-    
-
-
-#df = df.set_index('OBJECT_ID')
-#
-#
-#def get_text(c):
-#    soup = BeautifulSoup(c, 'html.parser')
-#    return soup.getText()
-#    #return re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！？、~@#￥%……&*（）【】《》℃丨“”(a-z)(0-9)(A-Z)：-]", "",t)
-#
-#
-#p = r"[\s+\.\!\/_,$%^*(+\"\']+|[+——！？、~@#￥%……&*（）【】《》℃丨“”(a-z)(0-9)(A-Z)：-]"
-#df['clean_text'] = df['CONTENT'].apply(lambda x: get_text(x))
-#df = df[~df.clean_text.str.contains('您没有浏览')]
-#df.clean_text = df.clean_text.apply(lambda x: re.sub(p,'',x))
-#df.TITLE = df.TITLE.apply(lambda x: re.sub(p,'', x))
-#
-#
-#print(df.columns)
-#print(df.head(5))
-#
-#df = df[['TITLE', 'WINDCODES', 'clean_text']]
-#tokenizer = hanlp.load('PKU_NAME_MERGED_SIX_MONTHS_CONVSEG')
-##sen_splitter = hanlp.utils.rules.split_sentence
-#
-#for i in range(len(df)):
-#    idx = df.index[i]
-#    a = tokenizer(df.TITLE.values.tolist()[i])
-#    b = tokenizer(df.clean_text.values.tolist()[i])
-#    t = np.array([idx, a, b]).reshape(1, -1)
-#    t_df = pd.DataFrame(t, columns=['OBJECT_ID','title_token', 'text_token'])
-#    if i == 0:
-#        t_df.to_csv('token.csv', mode='w', header=True, index=False)
-#    else:
-#        t_df.to_csv('token.csv', mode='a', header=False, index=False)
-#
